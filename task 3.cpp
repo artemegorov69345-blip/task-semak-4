2e4b6ddcb23902f2137d34646f297765b96bfc8b
Артем Егоров ТКИ-242 <artemegorov69345@gmail.com>
2026-09-16 14:35:07 +0300

    исправлено

--- task 3.cpp
+++ task 3.cpp
@@ -6,9 +6,31 @@
 using namespace std;
 
 int main() {
-    vector<int> V1 = {1, 2, 2, 3, 5};
-    vector<int> V2 = {2, 3, 3, 4, 6};
+    // Ввод первого вектора с клавиатуры
+    int n1;
+    cout << "Введите размер первого вектора: ";
+    cin >> n1;
+    vector<int> V1;
+    cout << "Введите " << n1 << " чисел: ";
+    for (int i = 0; i < n1; ++i) {
+        int x;
+        cin >> x;
+        V1.push_back(x);
+    }
 
+    // Ввод второго вектора с клавиатуры
+    int n2;
+    cout << "Введите размер второго вектора: ";
+    cin >> n2;
+    vector<int> V2;
+    cout << "Введите " << n2 << " чисел: ";
+    for (int i = 0; i < n2; ++i) {
+        int x;
+        cin >> x;
+        V2.push_back(x);
+    }
+
+    // Дальше всё остаётся без изменений
     multiset<int> M1(V1.begin(), V1.end());
     multiset<int> M2(V2.begin(), V2.end());
     multiset<int> M;
