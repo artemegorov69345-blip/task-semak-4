27d62328ac05024f873e909fc1d9406f19945acf
Артем Егоров ТКИ-242 <artemegorov69345@gmail.com>
2026-09-18 17:27:44 +0300

    Исправлено

--- task 2.cpp
+++ task 2.cpp
@@ -7,68 +7,55 @@
 
 using namespace std;
 
+// Умножает первый, средний и последний элементы контейнера на 2
+template <typename Container>
+void modifyMiddleElements(Container& c) {
+    c.front() *= 2;
+    auto it = c.begin();
+    advance(it, c.size() / 2);
+    *it *= 2;
+    c.back() *= 2;
+}
+
+// Выводит контейнер на экран с подписью
+template <typename Container>
+void printContainer(const Container& c, const string& name) {
+    cout << name << ": ";
+    for_each(c.begin(), c.end(), [](int n) {
+        cout << n << " ";
+    });
+    cout << endl;
+}
+
 int main() {
     vector<int> V;
     int nV;
     cout << "Введите размер вектора (нечетное, >=3): ";
-    if (!(cin >> nV) || nV < 3 || nV % 2 == 0) {
-        cout << "Ошибка: введено некорректное число." << endl;
-        return 1;
-    }
+    cin >> nV;
     cout << "Введите " << nV << " чисел: ";
     copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(V));
 
     deque<int> D;
     int nD;
     cout << "Введите размер дека (нечетное, >=3): ";
-    if (!(cin >> nD) || nD < 3 || nD % 2 == 0) {
-        cout << "Ошибка: введено некорректное число." << endl;
-        return 1;
-    }
+    cin >> nD;
     cout << "Введите " << nD << " чисел: ";
     copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(D));
 
     list<int> L;
     int nL;
     cout << "Введите размер списка (нечетное, >=3): ";
-    if (!(cin >> nL) || nL < 3 || nL % 2 == 0) {
-        cout << "Ошибка: введено некорректное число." << endl;
-        return 1;
-    }
+    cin >> nL;
     cout << "Введите " << nL << " чисел: ";
     copy(istream_iterator<int>(cin), istream_iterator<int>(), back_inserter(L));
 
-    V.front() *= 2;
-    V[V.size() / 2] *= 2;
-    V.back() *= 2;
+    modifyMiddleElements(V);
+    modifyMiddleElements(D);
+    modifyMiddleElements(L);
 
-    D.front() *= 2;
-    D[D.size() / 2] *= 2;
-    D.back() *= 2;
-
-    L.front() *= 2;
-    auto it = L.begin();
-    advance(it, L.size() / 2);
-    *it *= 2;
-    L.back() *= 2;
-
-    cout << "Вектор: ";
-    for_each(V.begin(), V.end(), [](int n) {
-        cout << n << " ";
-    });
-    cout << endl;
-
-    cout << "Дек: ";
-    for_each(D.begin(), D.end(), [](int n) {
-        cout << n << " ";
-    });
-    cout << endl;
-
-    cout << "Список: ";
-    for_each(L.begin(), L.end(), [](int n) {
-        cout << n << " ";
-    });
-    cout << endl;
+    printContainer(V, "Вектор");
+    printContainer(D, "Дек");
+    printContainer(L, "Список");
 
     return 0;
 }
